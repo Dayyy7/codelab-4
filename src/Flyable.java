18e68faa1d1cc0565f6ee4076f3a6ad0d7848c99
udaywakid1@gmail.com <udaywakid1@gmail.com>
2024-04-08 00:19:08 +0700

    first commit

---
+++ src/Flyable.java
@@ -0,0 +1,8 @@
+package kendaraan.util;
+
+public interface Flyable {
+    default void fly() {
+
+        System.out.println("Flying...");
+    }
+}
---
+++ src/Shootable.java
@@ -0,0 +1,5 @@
+package kendaraan.util;
+
+public interface Shootable {
+    void shoot(String vehicle);
+}
